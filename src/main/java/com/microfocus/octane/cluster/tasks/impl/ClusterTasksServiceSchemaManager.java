58d366ea5d8ca24a6d38dd6ea79ba1a2a8b9afb1
gullery <yuri.guller@hpe.com>
2017-09-27 23:39:04 +0300

    tech: adding flyway management support (SQL)

--- src/main/java/com/microfocus/octane/cluster/tasks/impl/ClusterTasksServiceSchemaManager.java
+++ src/main/java/com/microfocus/octane/cluster/tasks/impl/ClusterTasksServiceSchemaManager.java
@@ -2,12 +2,16 @@ package com.microfocus.octane.cluster.tasks.impl;
 
 import com.microfocus.octane.cluster.tasks.api.ClusterTasksServiceConfigurerSPI;
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 
 @Service
 class ClusterTasksServiceSchemaManager {
+	private final Logger logger = LoggerFactory.getLogger(ClusterTasksServiceSchemaManager.class);
+
 	private static final String CTS_SCHEMA_HISTORY_TABLE_NAME = "CTS_SCHEMA_HISTORY";
 	private static final String SQL_MIGRATION_PREFIX = "v";
 
@@ -19,13 +23,17 @@ class ClusterTasksServiceSchemaManager {
 			throw new IllegalArgumentException("DataSource MUST NOT be null");
 		}
 
-		Flyway flyway = new Flyway();
-		flyway.setDataSource(dataSource);
-		flyway.setTable(CTS_SCHEMA_HISTORY_TABLE_NAME);
-		flyway.setSqlMigrationPrefix(SQL_MIGRATION_PREFIX);
-		flyway.setBaselineOnMigrate(true);
-		flyway.setLocations(getSQLsLocation(dbType));
-		flyway.migrate();
+		try {
+			Flyway flyway = new Flyway();
+			flyway.setDataSource(dataSource);
+			flyway.setTable(CTS_SCHEMA_HISTORY_TABLE_NAME);
+			flyway.setSqlMigrationPrefix(SQL_MIGRATION_PREFIX);
+			flyway.setBaselineOnMigrate(true);
+			flyway.setLocations(getSQLsLocation(dbType));
+			flyway.migrate();
+		} catch (Exception e) {
+			logger.error("DB maintenance failed", e);
+		}
 	}
 
 	private String getSQLsLocation(ClusterTasksServiceConfigurerSPI.DBType dbType) {
