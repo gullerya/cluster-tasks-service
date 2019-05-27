8f6b44c36abd5421a376366e10accae057e38518
gullery <gullerya@gmail.com>
2019-05-27 18:23:51 +0300

    issue #18: adding tests

--- src/main/java/com/microfocus/cluster/tasks/impl/ClusterTasksServiceImpl.java
+++ src/main/java/com/microfocus/cluster/tasks/impl/ClusterTasksServiceImpl.java
@@ -25,11 +25,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 /**
@@ -238,6 +234,13 @@ public class ClusterTasksServiceImpl implements ClusterTasksService {
 
 	private void initService() {
 		logger.info("starting initialization");
+		if (serviceConfigurer.getDbType() == null) {
+			throw new IllegalStateException("DB type MUST NOT be NULL");
+		}
+		if (serviceConfigurer.getDataSource() == null) {
+			throw new IllegalStateException("DataSource MUST NOT be NULL");
+		}
+
 		if (serviceConfigurer.getAdministrativeDataSource() != null) {
 			logger.info("performing schema maintenance");
 			schemaManager.executeSchemaMaintenance(serviceConfigurer.getDbType(), serviceConfigurer.getAdministrativeDataSource());
