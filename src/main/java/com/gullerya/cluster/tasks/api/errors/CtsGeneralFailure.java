9acb557b3bbb4121db3870354fddf4c44c7e0d1f
gullerya <gullerya@gmail.com>
2019-06-30 22:44:44 +0300

    added serial version ID as advised for the serializables

--- src/main/java/com/gullerya/cluster/tasks/api/errors/CtsGeneralFailure.java
+++ src/main/java/com/gullerya/cluster/tasks/api/errors/CtsGeneralFailure.java
@@ -9,6 +9,8 @@
 package com.gullerya.cluster.tasks.api.errors;
 
 public class CtsGeneralFailure extends RuntimeException {
+	static final long serialVersionUID = -7124807193745766948L;
+
 	public CtsGeneralFailure(String message, Throwable throwable) {
 		super(message, throwable);
 	}
--- src/main/java/com/gullerya/cluster/tasks/api/errors/CtsSqlFailure.java
+++ src/main/java/com/gullerya/cluster/tasks/api/errors/CtsSqlFailure.java
@@ -11,6 +11,8 @@ package com.gullerya.cluster.tasks.api.errors;
 import java.sql.SQLException;
 
 public class CtsSqlFailure extends RuntimeException {
+	static final long serialVersionUID = -7024897193745766949L;
+
 	public CtsSqlFailure(String message, SQLException sqle) {
 		super(message, sqle);
 	}
